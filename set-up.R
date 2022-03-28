910fda0108dcfb292801840b7a6723939ee6ac4c
szeller42 <s.zeller@posteo.net>
2022-03-28 16:24:13 +0200

    📦️ add necessary packages

---
+++ set-up.R
@@ -0,0 +1,12 @@
+############################################
+#Project: Inconsistent Writing in Word files
+#Author: Sarah
+#Data: Scribbr commissions
+#last edit: 22/03/28
+#############################################
+
+####NECESSARY packages
+library(officer)
+library(data.table)
+library(tidytext)
+library(stringr)
