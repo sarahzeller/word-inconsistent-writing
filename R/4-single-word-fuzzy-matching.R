e4f2ef4ab4d5966b7e9b18b227a5215ea0031d99
szeller42 <s.zeller@posteo.net>
2022-05-03 16:58:52 +0200

    ⚡️ only include pairs with likely typos

--- R/4-single-word-fuzzy-matching.R
+++ R/4-single-word-fuzzy-matching.R
@@ -11,19 +11,30 @@ source("set-up.R")
 words_with_infoDT <- readRDS("output/upper_lowerDT.rds")
 
 library(stringdist)
-# build a DT in which all words are confronted with the other
+library(stringr)
+# build a DT in which all words are confronted with all others with the same 
+# first letter
 wordsDT <- words_with_infoDT[, "word"][
   CJ(word1 = word,
-     word2 = word,
+     other_word = word,
      unique = TRUE)
-  # exclude words which are paired with themselves
-][word != word2][
-  # calculate distance between words
-  , dist := stringdist(word, word2)
 ][
-  # exclude too large distances;
+  # exclude pairs with the different start letter
+  , c("word1_letter1", "word2_letter1") := 
+                     .(substr(word, 1, 1), substr(other_word, 1, 1))
+][
+  word1_letter1 == word2_letter1
+][
+ , c("word1_letter1", "word2_letter1") := NULL 
+][
+  # exclude pairs with the same word
+  word != other_word][
+  # calculate distance between words
+  , dist := stringdist(word, other_word)
+][
+  # exclude too short words & too large distances
   # TODO figure out a good distance
-  dist < 3
+  length(word) > dist + 1 & dist < 4
 ]
 
 
